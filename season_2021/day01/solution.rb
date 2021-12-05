db53a2e32f2b257c0f05525dd5850bae8763b016
Borislav Radev <borislav.radev@receipt-bank.com>
2021-12-05 22:55:05 +0200

    Day01 Part & Part2 solutions

---
+++ season_2021/day01/solution.rb
@@ -0,0 +1,56 @@
+module Season2021
+  module Day01
+    class << self
+      def input
+        File.read("#{__dir__}/input.txt").split.map &:to_i
+      end
+    end
+
+    class Part1
+      def execute
+        @input = Day01.input
+        @larger_mesasurements_count = 0
+        count_larger_mesasurements
+
+        puts "Day01, Part1, Result: #{@larger_mesasurements_count}"
+      end
+
+      private
+
+      def count_larger_mesasurements
+        (1...@input.size).each do |index|
+          @larger_mesasurements_count += 1 if @input[index] > @input[index - 1]
+        end
+      end
+    end
+
+    class Part2
+      def execute
+        @input = Day01.input
+        @larger_sum_mesasurements_count = 0
+        count_larger_mesasurements
+
+        puts "Day01, Part2, Result: #{@larger_sum_mesasurements_count}"
+      end
+
+      private
+
+      def count_larger_mesasurements
+        current_sum = nil
+        prev_sum = nil
+        last_index = @input.size - 1
+
+        (1...last_index).each do |index|
+          current_sum = @input[index - 1] + @input[index] + @input[index + 1]
+          @larger_sum_mesasurements_count += 1 if prev_sum && (current_sum > prev_sum)
+          prev_sum = current_sum
+        end
+      end
+    end
+  end
+end
+
+Season2021::Day01::Part1.new.execute
+Season2021::Day01::Part2.new.execute
+
+
